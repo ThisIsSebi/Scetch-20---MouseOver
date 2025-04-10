ed81a140b4b7380f47be885753ebff27df2872aa
ThisIsSebi <shoff439@gmail.com>
2025-04-10 22:47:58 +0200

    changed it to original state

--- sketch.js
+++ sketch.js
@@ -7,29 +7,27 @@ let colourZ;
 
 function setup() {
   createCanvas(600, 400);
-  for(let i = 0; i < 5; i++){
+  for (let i = 0; i < 10; i++) {
     let x = random(width);
     let y = random(height);
-    let r = random(10, 50);
-    colourX = random(0, 255);
-    colourY = random(255);
-    colourZ = random(255);
-    let b  = new Bubble(x, y, r, colourX, colourY, colourZ);
+    let r = random(20, 60);
+    // colourX = random(0, 255);
+    // colourY = random(255);
+    // colourZ = random(255);
+    let b = new Bubble(x, y, r);
     bubbles.push(b);
   }
   button = createButton("Clear");
 }
 
 function mousePressed() {
-  for(let i = 0; i < bubbles.length; i++){
+  for (let i = 0; i < bubbles.length; i++) {
     bubbles[i].clicked(mouseX, mouseY);
   }
-  bubble.clicked();  
-  window.location.reload();
 }
 
 // function mousePressed() {
-// 
+//
 // }
 
 function draw() {
@@ -41,11 +39,25 @@ function draw() {
 }
 
 class Bubble {
-  constructor(x, y, r, colourX, colourY, colourZ) {
+  constructor(x, y, r) {
     this.x = x;
     this.y = y;
     this.r = r;
-   this.brightness = 0;
+    // this.colourX = colourX;
+    // this.colourY = colourY;
+    // this.colourZ = colourZ;
+    this.brightness = 0;
+  }
+
+  clicked(x, y) {
+    let d = dist(x, y, this.x, this.y);
+    if (d < this.r) {
+      // this.colourX = random(255);
+      // this.colourY = random(255);
+      // this.colourZ = random(255);
+      this.brightness = 255;
+      console.log("clicked");
+    }
   }
 
   move() {
@@ -56,16 +68,10 @@ class Bubble {
   show() {
     stroke(255);
     strokeWeight(4);
-    fill(this.brightness, 125)
+    fill(this.brightness, 125);
     // fill(this.colourX, this.colourY, this.colourZ);
     ellipse(this.x, this.y, this.r * 2);
   }
 
-  clicked(x,y){
-    let d = dist(x, y, this.x, this.y);
-    if(d < this.r){
-      this.brightness = 255;
-      console.log("clicked");
-    }
-  }
+
 }
