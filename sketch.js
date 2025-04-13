cf25cdd158e339745f6f2e1ae61eb69af0d60c55
ThisIsSebi <shoff439@gmail.com>
2025-04-13 22:38:28 +0200

    new bubble is created

--- sketch.js
+++ sketch.js
@@ -7,30 +7,30 @@ let colourZ;
 
 function setup() {
   createCanvas(600, 400);
-  createBubbles();
+  // createBubbles();
   let button = createButton("clear");
   button.mousePressed(clearData);
 }
 
 function mousePressed() {
-  for (let i = 0; i < bubbles.length; i++) {
-    bubbles[i].clicked(mouseX, mouseY);
-  }
+  let r = random(10, 50);
+  let b = new Bubble(mouseX, mouseY, r);
+  bubbles.push(b);
 }
 
-function createBubbles() {
-  for (let i = 0; i < 5; i++) {
-    let x = random(width);
-    let y = random(height);
-    let r = random(20, 60);
-    colourX = 0;
-    colourY = 0;
-    colourZ = 0;
-    fill(0);
-    let b = new Bubble(x, y, r);
-    bubbles.push(b);
-  }
-}
+// function createBubbles() {
+//   for (let i = 0; i < 5; i++) {
+//     let x = random(width);
+//     let y = random(height);
+//     let r = random(20, 60);
+//     colourX = 0;
+//     colourY = 0;
+//     colourZ = 0;
+//     fill(0);
+//     let b = new Bubble(x, y, r);
+//     bubbles.push(b);
+//   }
+// }
 
 function clearData() {
   window.location.reload();
